78e65ce8e92a3ed8d67fc9aa969d5fcb6aedc62c
Caminha <gabrielcaminha255@gmail.com>
2022-02-22 01:15:22 -0300

    exercises 6.2 almost completed, bootstrap site in day 6.1 folder

---
+++ fundamentos/bloco-6-forms-flexbox-e-responsivo/dia-1-forms/exercicios/afterFramework/datepicker.js
@@ -0,0 +1,8 @@
+let datePicker = document.getElementById('datepicker');
+
+datePicker.DatePickerX.init();
+
+datePickerDiv = document.querySelector('.date-picker-x');
+console.log(datePickerDiv);
+
+datePickerDiv.setAttribute('id','datePickerDiv');
