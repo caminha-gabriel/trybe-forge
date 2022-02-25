9be415cc604bd7dea61ea8729f2fcee731a12cba
Caminha <gabrielcaminha255@gmail.com>
2022-02-25 02:23:49 -0300

    first breakpoint almost completed; script.js added

---
+++ fundamentos/bloco-6-forms-flexbox-e-responsivo/dia-5--responsivo-e-mobile-first/exercicios/exercise2/script.js
@@ -0,0 +1,7 @@
+let menuMobile = document.querySelector('i');
+let menuCollapse = document.querySelector('.collapse');
+
+menuMobile.addEventListener('click', function () {
+  menuCollapse.classList.toggle('collapse');
+  console.log(menuCollapse);
+})
