04131f05aea83f4f9b006468beb96227b934b1da
Caminha <gabrielcaminha255@gmail.com>
2022-03-04 17:05:44 -0300

    part 3 fixation

---
+++ fundamentos/bloco-7-ecmascript6-e-testes-unitários/dia-2-fluxo-de-excecao-e-objetos/fixacao/parte3/objectValues.js
@@ -0,0 +1,22 @@
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+// for (const property in coolestTvShow) {
+//   console.log(property);
+// }
+
+// BoJack Horseman
+// adult animation
+// Raphael Bob-Waksberg
+// Princess Carolyn
+// Princess Carolyn always lands on her feet.
+// 6
+
+
+console.log(Object.values(coolestTvShow));
