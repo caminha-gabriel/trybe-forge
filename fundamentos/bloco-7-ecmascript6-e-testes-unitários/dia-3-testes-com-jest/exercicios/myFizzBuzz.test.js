e22415685d34a927a919609282fd84c9604ef3bf
Caminha <gabrielcaminha255@gmail.com>
2022-03-07 20:26:21 -0300

    function 3 tests completed

--- fundamentos/bloco-7-ecmascript6-e-testes-unitários/dia-3-testes-com-jest/exercicios/myFizzBuzz.test.js
+++ fundamentos/bloco-7-ecmascript6-e-testes-unitários/dia-3-testes-com-jest/exercicios/myFizzBuzz.test.js
@@ -1,1 +1,23 @@
 const myFizzBuzz = require('./myFizzBuzz');
+
+describe('Tests myFizzBuzz function', () => {
+  it('returns fizzbuzz when myFizzBuzz(15)', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+  });
+  
+  it('returns fizz when myFizzBuzz(9)', () => {
+    expect(myFizzBuzz(9)).toEqual('fizz');
+  });
+
+  it('returns buzz when myFizzBuzz(20)', () => {
+    expect(myFizzBuzz(20)).toEqual('buzz');
+  });
+
+  it('returns 4 when myFizzBuzz(4)', () => {
+    expect(myFizzBuzz(4)).toEqual(4);
+  });
+
+  it('returns false when myFizzBuzz("ring")', () => {
+    expect(myFizzBuzz('ring')).toEqual(false);
+  });
+});
