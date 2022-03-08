e59e4ed7c2802b0774badd85f3efaea6e958309e
Caminha <gabrielcaminha255@gmail.com>
2022-03-08 20:53:51 -0300

    exercise completed

--- fundamentos/bloco-5-js-dom-e-seletores/dia-4-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-js-dom-e-seletores/dia-4-web-storage/exercicios/script.js
@@ -1,7 +1,10 @@
-let body = document.body;
-let selectBgColor = document.getElementById('bgColor');
-let selectTextColor = document.getElementById('textColor');
-let selectFontSize = document.getElementById('fontSize');
+const body = document.body;
+body.style.fontSize = '26px';
+const selectBgColor = document.getElementById('bgColor');
+const selectTextColor = document.getElementById('textColor');
+const selectFontSize = document.getElementById('fontSize');
+const selectLineHeight = document.getElementById('lineHeight');
+const selectFontFamily = document.getElementById('fontFamily');
 
 selectBgColor.addEventListener('click', (event) => {
   const color = event.target.value;
@@ -52,11 +55,58 @@ switch (color) {
   break;
 
   case 'purple':
-  body.style.color = 'purple'
+  body.style.color = 'purple';
+  break;
+
+  case 'red':
+  body.style.color = 'red';
   break;
   }
 });
+
 selectFontSize.addEventListener('keyup', (event) => {
   const size = event.target.value;
   body.style.fontSize = size +'px';
-})
+
+});
+
+selectLineHeight.addEventListener('click', (event) => {
+  const height = event.target.value;
+  console.log(height);
+  switch (height) {
+    case 'normal':
+      body.style.lineHeight = 'normal';
+      break;
+  
+    case '0.8x':
+      body.style.lineHeight = '0.8';
+      break;
+
+    case '1.6x':
+      body.style.lineHeight = '1.6';
+      break;
+
+    case '2.4x':
+      body.style.lineHeight = '2.4';
+      break;
+
+    default:
+      break;
+  }
+});
+
+selectFontFamily.addEventListener('click', (event) => {
+  const font = event.target.value;
+  switch (font) {
+    case 'verdana':
+      body.style.fontFamily = 'verdana';
+      break;
+
+    case 'helvetica':
+      body.style.fontFamily = 'helvetica';
+      break;
+
+    default:
+      break;
+  }
+});
