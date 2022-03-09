3eeb0f18152093c12aec4e960c91006421e68fb7
Caminha <gabrielcaminha255@gmail.com>
2022-03-08 22:14:26 -0300

    some changes on files; deleted datepickerX to use PikaDay; ignoring node_modules file

--- fundamentos/bloco-6-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks/exercicios/afterFramework/datepicker.js
+++
@@ -1,8 +0,0 @@
-let datePicker = document.getElementById('datepicker');
-
-datePicker.DatePickerX.init();
-
-datePickerDiv = document.querySelector('.date-picker-x');
-console.log(datePickerDiv);
-
-datePickerDiv.setAttribute('id','datePickerDiv');
