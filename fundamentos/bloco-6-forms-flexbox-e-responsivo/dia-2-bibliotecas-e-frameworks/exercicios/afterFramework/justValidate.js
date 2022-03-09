5d700101545334f9e6df4de7715ff056c7c99069
Caminha <gabrielcaminha255@gmail.com>
2022-03-08 22:31:07 -0300

    exercise completed, validations finished

--- fundamentos/bloco-6-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks/exercicios/afterFramework/justValidate.js
+++ fundamentos/bloco-6-forms-flexbox-e-responsivo/dia-2-bibliotecas-e-frameworks/exercicios/afterFramework/justValidate.js
@@ -4,6 +4,10 @@ const validation = new JustValidate('#form');
 
 validation.addField('#name-input', [
   {
+    rule: 'required',
+    errorMessage: 'Nome obrigatório',
+  },
+  {
     rule: 'minLength',
     value: 4,
   },
@@ -12,3 +16,37 @@ validation.addField('#name-input', [
     value: 32,
   },
 ])
+.addField('#email', [
+  {
+    rule: 'required',
+    errorMessage: 'Email obrigatório',
+  },
+  {
+    rule: 'email',
+    errorMessage: 'Email inválido!',
+  },
+  {
+    rule: 'minLength',
+    value: 10,
+  },
+  {
+    rule: 'maxLength',
+    value: 50,
+  },
+])
+.addField('#textArea', [
+  {
+    rule: 'required',
+    errorMessage: 'Nos conte um pouco sobre você!'
+  },
+  {
+    rule: 'maxLength',
+    value: 500,
+  },
+])
+.addField('#consent-image', [
+  {
+    rule: 'required',
+    errorMessage: 'Precisamos do seu consenso para essa viagem',
+  }
+]);
