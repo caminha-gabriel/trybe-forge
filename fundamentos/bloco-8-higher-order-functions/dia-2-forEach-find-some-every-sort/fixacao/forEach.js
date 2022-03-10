8c8cc01ce26be168dcf8cc28737de53f4c2036f9
Caminha <gabrielcaminha255@gmail.com>
2022-03-10 15:30:08 -0300

    fixation day 8.2 started, first fixation completed

---
+++ fundamentos/bloco-8-higher-order-functions/dia-2-forEach-find-some-every-sort/fixacao/forEach.js
@@ -0,0 +1,16 @@
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach(showEmailList);
+
+// 2 - Leia e entenda este exemplo: https://codepen.io/pen/?template=LYZPEwV forEach feito no CodePen.
