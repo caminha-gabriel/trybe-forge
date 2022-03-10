5624064ae2d7a61666fa44189fb64d09b36aed3b
Caminha <gabrielcaminha255@gmail.com>
2022-03-10 02:32:17 -0300

    comment correction

--- fundamentos/bloco-8-higher-order-functions/dia-1-introducaoo-a-HOF/exercicios/script.js
+++ fundamentos/bloco-8-higher-order-functions/dia-1-introducaoo-a-HOF/exercicios/script.js
@@ -18,16 +18,7 @@ const newEmployees = (funct) => {
 };
 
 console.log(newEmployees(newHired));
-// const newEmployees = (newHired) => {
-//   const employees = {
-//     id1: `${newHired('Pedro Guerra')}`, // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-//     id2: newHired('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-//     id3: newHired('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-//   }
-//   return employees;
-// };
 
-// console.log(newEmployees());
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
 // 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
