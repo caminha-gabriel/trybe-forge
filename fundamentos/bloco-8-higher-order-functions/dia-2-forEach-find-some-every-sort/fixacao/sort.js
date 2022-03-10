605aa7b951b45344031692ffa71bbde03d2ea5ab
Caminha <gabrielcaminha255@gmail.com>
2022-03-10 19:01:06 -0300

    final fixation completed

---
+++ fundamentos/bloco-8-higher-order-functions/dia-2-forEach-find-some-every-sort/fixacao/sort.js
@@ -0,0 +1,21 @@
+// 1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const sortArray = (array) => {
+  // Sort array of objects found at https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+  array.sort((a, b) => {
+    return a.age - b.age;
+});
+
+}
+
+sortArray(people);
+console.log(people);
+
+// 2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente .
