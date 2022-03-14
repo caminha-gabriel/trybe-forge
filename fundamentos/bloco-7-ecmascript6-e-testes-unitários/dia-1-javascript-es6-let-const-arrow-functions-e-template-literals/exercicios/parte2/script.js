9a6ce162cde7dbf5273f0751ec59cd9c3c5879d9
Caminha <gabrielcaminha255@gmail.com>
2022-03-14 00:44:18 -0300

    changes to exercise 1 of part2

--- fundamentos/bloco-7-ecmascript6-e-testes-unitários/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/parte2/script.js
+++ fundamentos/bloco-7-ecmascript6-e-testes-unitários/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/parte2/script.js
@@ -1,15 +1,16 @@
-// const returnFatorial = number => {
-//   let sum = number;
-//   for (let i = number - 1; i > 1; i--) {
-//     sum = sum * i
-//   }
-//   if (number === 0) sum = 1;
-//   return sum
-// }
+const returnFatorial = number => {
+  let fatorial = number;
+  if (number === 0) fatorial = 1;
+  for (let i = number - 1; i > 1; i--) {
+    fatorial *= i;
+  }
+  return fatorial;
+}
 
-// console.log(returnFatorial(0));
+console.log(returnFatorial(3));
 
+// Added using:
+//  http://devfuria.com.br/logica-de-programacao/recursividade-fatorial/
+// const returnFatorial = number => number === 0 ? 1 : number * returnFatorial(number - 1);
 
-const returnFatorial = number => number === 0 ? 1 : number * returnFatorial(number - 1);
-
-console.log(returnFatorial(4));
+// console.log(returnFatorial(5));
