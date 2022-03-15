94756d2c237494acd87882d3904e7562841e058c
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 00:42:55 -0300

    fixation 2 completed

---
+++ fundamentos/bloco-8-higher-order-functions/dia-4-reduce/fixacao/fixation2.js
@@ -0,0 +1,9 @@
+// Para fixar ainda mais conceito de reduce , faça uma função que some todos os números pares do array:
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const pairsOf = (array) => array.filter((el) => !(el % 2));
+
+const sumOf = ((array) => array.reduce((acc, cur) => acc + cur));
+
+console.log(sumOf(pairsOf(numbers)));
