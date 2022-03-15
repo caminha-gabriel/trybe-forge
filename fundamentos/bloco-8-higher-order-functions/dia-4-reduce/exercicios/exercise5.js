cfdd7c1788eb3eca0f06b21c9003b4dd378709c5
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 04:44:43 -0300

    exercise 5 completed with dinamic solution

--- fundamentos/bloco-8-higher-order-functions/dia-4-reduce/exercicios/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions/dia-4-reduce/exercicios/exercise5.js
@@ -1,11 +1,22 @@
-// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
-
 const names = [
   'Aanemarie', 'Adervandes', 'Akifusa',
   'Abegildo', 'Adicellia', 'Aladonata',
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
-function containsA() {
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
   // escreva seu código aqui
-}
+const arrayOfLowerConcat = () => {
+  let concat = names.reduce((acc, curr) => {
+    return acc.concat(curr);
+  });
+  return concat.toLowerCase().split('');
+};
+
+const countCharacter = (array, character) => {
+  let counter = 0;
+  array.forEach((element) => element === character ? counter++ : undefined);
+  return `Your array has ${counter} of the "${character}" character.`;
+}; 
+console.log(countCharacter(arrayOfLowerConcat(), 'a'));
