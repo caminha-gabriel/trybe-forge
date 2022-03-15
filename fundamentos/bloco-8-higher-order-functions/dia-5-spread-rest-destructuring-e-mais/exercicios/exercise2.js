974a5e16a2a81bb80423d19dc85ccfd33cf77e79
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 18:45:27 -0300

    exercise 2 completed

--- fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise2.js
@@ -1,2 +1,8 @@
 // 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 // Dica: use parâmetro rest .
+
+function sum (...rest) {
+  return rest.reduce((acc, curr) => acc + curr)
+};
+
+console.log(sum(2, 4, 5, 1 ,2 ,1 ,2));
