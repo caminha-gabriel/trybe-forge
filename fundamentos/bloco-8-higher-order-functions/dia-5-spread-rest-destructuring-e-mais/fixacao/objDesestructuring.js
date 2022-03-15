32327b56c161042d8028ae6fa2d90acff93c8789
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 15:08:15 -0300

    object desestructuring fixation added

---
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/fixacao/objDesestructuring.js
@@ -0,0 +1,40 @@
+// const daysOfWeek = {
+//   workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+//   weekend: ['Saturday', 'Sunday'],
+// };
+
+// const { workDays, weekend } = daysOfWeek;
+// console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+// console.log(weekend); // ['Saturday', 'Sunday']
+
+// const weekdays = [...workDays, ...weekend];
+// console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+// const product = {
+//   name: 'Smart TV Crystal UHD',
+//   price: '1899.05',
+//   seller: 'Casas de Minas',
+// };
+
+// const printProductDetails = ({ name, price, seller }) => {
+//   console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+// };
+
+// printProductDetails(product); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
+
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const professional = {...user, ...jobInfos,};
+const {name: nome, age, nationality, profession, squad, squadInitials} = professional;
+console.log(`Hi, I'm ${nome}, I'm ${age} years old and I'm ${nationality}. I work at ${squad}(${squadInitials}) as a ${profession} and I'm very excited to apply for your work!`);
