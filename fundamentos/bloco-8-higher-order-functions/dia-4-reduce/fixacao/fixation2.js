787c742169d31afdaf22e6e6d5b630f52e937e56
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 00:49:01 -0300

    comments added

--- fundamentos/bloco-8-higher-order-functions/dia-4-reduce/fixacao/fixation2.js
+++ fundamentos/bloco-8-higher-order-functions/dia-4-reduce/fixacao/fixation2.js
@@ -1,5 +1,7 @@
 // Para fixar ainda mais conceito de reduce , faça uma função que some todos os números pares do array:
 
+// Solução usando filter e reduce :
+
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 const pairsOf = (array) => array.filter((el) => !(el % 2));
@@ -7,6 +9,8 @@ const pairsOf = (array) => array.filter((el) => !(el % 2));
 const sumOf = ((array) => array.reduce((acc, cur) => acc + cur));
 
 console.log(sumOf(pairsOf(numbers)));
+
+// Solução usando apenas reduce :
 
 const pairsWithReduceOnly = (array) => array.reduce((acc, cur) => cur % 2 === 0 ? acc + cur : acc);
 
