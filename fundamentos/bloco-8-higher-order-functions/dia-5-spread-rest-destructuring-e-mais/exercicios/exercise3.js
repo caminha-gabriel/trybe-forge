173f2e2edf0ad747f63b39fed385203b15d2cd92
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 18:51:32 -0300

    exercise 3 completed

--- fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise3.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise3.js
@@ -16,7 +16,9 @@ const gunnar = {
 };
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
-
+const personLikes = (person) => {
+  const { name, age, likes } = person
+  return`${name} is ${age} years old and likes ${likes.join(', ')}.`;
+}
 console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
 console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
