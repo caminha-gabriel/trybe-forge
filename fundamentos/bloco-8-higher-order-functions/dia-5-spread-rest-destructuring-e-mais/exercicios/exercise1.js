cfba1131fa9b21e50e82ddb4000064e70a295a2f
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 18:43:52 -0300

    exercise 1 completed

--- fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise1.js
@@ -8,7 +8,7 @@ const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
-  rectangleArea() // altere a chamada da funcao rectangleArea
-  console.log(rectangle[0] * rectangle[1]);
+rectangles.forEach((rectangle, ind) => {
+  const area = rectangleArea(...rectangle);
+  console.log(`rectangle${ind+1} has area ${area}`);
 });
