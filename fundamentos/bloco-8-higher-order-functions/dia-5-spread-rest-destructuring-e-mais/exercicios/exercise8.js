8e5dfbe28d0420ace2475020e71067ba033e590f
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 19:47:23 -0300

    exercise 8 completed

--- fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise8.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise8.js
@@ -3,6 +3,8 @@
 
 // escreva greet abaixo
 
+const greet = (name, message = 'Hi') => `${message} ${name}!`;
+
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
