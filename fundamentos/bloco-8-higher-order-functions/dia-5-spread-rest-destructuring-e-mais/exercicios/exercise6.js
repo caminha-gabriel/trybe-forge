673c2c7e01ca8b534e596a4963187cce48741ae1
Caminha <gabrielcaminha255@gmail.com>
2022-03-15 19:40:33 -0300

    exercise 6 completed

--- fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise6.js
+++ fundamentos/bloco-8-higher-order-functions/dia-5-spread-rest-destructuring-e-mais/exercicios/exercise6.js
@@ -6,3 +6,17 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+
+const toObject = (car) => {
+  const [ carName, manufacturer, releaseDate ] = car;
+  const carObj = {
+    carName, 
+    manufacturer, 
+    releaseDate,
+  }
+  return carObj;
+};
+
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));
