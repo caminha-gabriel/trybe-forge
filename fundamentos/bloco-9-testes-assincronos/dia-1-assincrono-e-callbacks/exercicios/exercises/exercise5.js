c0d4cf989619a53d543e1024c9d6e26b47940405
Caminha <gabrielcaminha255@gmail.com>
2022-03-22 01:58:23 -0300

    exercise 5 completed

--- fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise5.js
+++ fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise5.js
@@ -16,6 +16,12 @@ const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => {
+    const celsius = getMarsTemperature();
+    callback(celsius);
+  }, messageDelay())
+}
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
