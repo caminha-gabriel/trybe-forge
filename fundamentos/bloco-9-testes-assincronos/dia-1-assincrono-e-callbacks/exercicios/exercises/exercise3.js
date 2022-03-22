e33c90e71d13b5e971f059995bc94fa3d00d4876
Caminha <gabrielcaminha255@gmail.com>
2022-03-22 01:45:16 -0300

    exercise 3 completed (exercises 1 and 2 didn't need asked me to code

--- fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise3.js
+++ fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise3.js
@@ -1,14 +1,16 @@
 // 3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
 
 const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  console.log("Returned planet: ", mars);
+  setTimeout(() => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    console.log("Returned planet: ", mars);
+  }, 4000);
 };
 
 getPlanet(); // imprime Marte depois de 4 segundos
