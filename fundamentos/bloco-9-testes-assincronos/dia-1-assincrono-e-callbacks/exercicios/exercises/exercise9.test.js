1fd82613a539e8b0f27e51d9fed2e1d7886e964e
Caminha <gabrielcaminha255@gmail.com>
2022-03-23 03:18:13 -0300

    first test from exercise9 completed

--- fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise9.test.js
+++ fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise9.test.js
@@ -1,11 +1,23 @@
 // 9 - A fim de evitar que futuros treinadores sejam prejudicados, o Professor Carvalho pediu que você o ajude a escrever testes para o sistema que distribui os pokémons. Crie um novo arquivo .test.js ou .spec.js e copie o código abaixo. Complete os testes para a função getPokemonDetails de acordo com as especificações.
 
 // Verifique se a importação do arquivo correto está sendo feita.
-const { getPokemonDetails } = require("./get-pokemon-details");
+const { getPokemonDetails } = require("./exercise8.js");
 
 describe("A função getPokemonDetails", () => {
-  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", () => {
-    // Escreva aqui seu código
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const notAPokemon = (element) => element.name === 'Leafeon';
+    const errorResult = new Error('Não temos esse pokémon para você :(');
+
+    function testError(newError, result) {
+      try {
+        expect(newError).toEqual(errorResult);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
+
+    getPokemonDetails(notAPokemon, testError);
   });
 
   it("retorna um pokemon que existe no banco de dados", () => {
