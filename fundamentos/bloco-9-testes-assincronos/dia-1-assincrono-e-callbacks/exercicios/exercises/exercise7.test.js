aba65607b24c9520a8911c210d5f102dd220fa1b
Caminha <gabrielcaminha255@gmail.com>
2022-03-23 02:47:39 -0300

    renamed file to use jest; exercise 7 completed

--- fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise7.test.js
+++ fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise7.test.js
@@ -1,7 +1,21 @@
 // 7 - Escreva um teste que verifique a chamada da callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
 
+const { expect } = require("@jest/globals");
+
+
 const uppercase = (str, callback) => {
-  setTimeout(() => {
+   setTimeout(() => {
     callback(str.toUpperCase());
-  }, 500);
+  }, 2000);
 };
+
+test('Tests callback uppercase function', (done) => {
+  uppercase('carls', (result) => {
+    try {
+    expect(result).toBe('CARLS');
+    done();
+    }catch (error) {
+      done(error);
+    }
+  })
+})
