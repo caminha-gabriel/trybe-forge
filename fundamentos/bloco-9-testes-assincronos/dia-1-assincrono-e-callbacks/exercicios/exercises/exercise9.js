8fa814f583417b336b240dc2d050e22636ccbcfa
Caminha <gabrielcaminha255@gmail.com>
2022-03-23 02:58:36 -0300

    exercise 9 file added

---
+++ fundamentos/bloco-9-testes-assincronos/dia-1-assincrono-e-callbacks/exercicios/exercises/exercise9.js
@@ -0,0 +1,14 @@
+// 9 - A fim de evitar que futuros treinadores sejam prejudicados, o Professor Carvalho pediu que você o ajude a escrever testes para o sistema que distribui os pokémons. Crie um novo arquivo .test.js ou .spec.js e copie o código abaixo. Complete os testes para a função getPokemonDetails de acordo com as especificações.
+
+// Verifique se a importação do arquivo correto está sendo feita.
+const { getPokemonDetails } = require("./get-pokemon-details");
+
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", () => {
+    // Escreva aqui seu código
+  });
+
+  it("retorna um pokemon que existe no banco de dados", () => {
+    // Escreva aqui seu código
+  });
+});
