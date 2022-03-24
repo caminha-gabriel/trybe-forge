bb7f90139e6d90d1baec87fb187b137de3f7a22f
Caminha <gabrielcaminha255@gmail.com>
2022-03-24 15:43:14 -0300

    exercise1 from 9.2 completed; fetchJokes added

--- fundamentos/bloco-9-testes-assincronos/dia-2-fetch-api-e-async-await/exercicios/apiScript.js
+++ fundamentos/bloco-9-testes-assincronos/dia-2-fetch-api-e-async-await/exercicios/apiScript.js
@@ -2,7 +2,16 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const specObj = {
+    method: 'GET',
+    headers: { "Accept": 'application/json' }
+  };
+
+  fetch(API_URL, specObj)
+  .then((response) => response.json())
+  .then((data) => data.joke)
+  .then((joke) => document.getElementById('jokeContainer').innerHTML = joke)
+  .catch((error) => console.log(error));
 };
 
 window.onload = () => fetchJoke();
