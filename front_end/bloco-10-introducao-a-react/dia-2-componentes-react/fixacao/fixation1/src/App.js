70a11a3e17e78292924749608b8330c57279dab2
Caminha <gabrielcaminha255@gmail.com>
2022-04-06 02:25:45 -0300

    fixation 1 and 2 from day 10.2 completed

---
+++ front_end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Image from './image';
+
+function App() {
+  return (
+    <main>
+    <Image 
+    source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="cute cat staring"/>
+    </main>
+  );
+}
+
+export default App;
---
+++ front_end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao/fixation2/src/App.js
@@ -0,0 +1,39 @@
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
+import './App.css';
+import Order from './Order';
+
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    const itens = [headphone, energyDrink];
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        {itens.map((order) => (<Order order={order} />))}
+      </div>
+    );
+  }
+}
+
+export default App;
