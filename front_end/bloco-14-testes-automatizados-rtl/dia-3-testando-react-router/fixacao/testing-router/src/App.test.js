bd13ead2658c0b0233e239cbe2eaffac82756145
Caminha <gabrielcaminha255@gmail.com>
2022-05-06 23:57:08 -0300

    day 14.3 fixation exercises completed

---
+++ front_end/bloco-14-testes-automatizados-rtl/dia-3-testando-react-router/fixacao/testing-router/src/App.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import  { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App, { About } from './App';
+
+describe('teste da aplicação toda', () => {
+
+  it('deve renderizar o componente App', () => {
+    renderWithRouter(<App />);
+  
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+  
+  it('deve renderizar o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+  
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+  
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  
+    const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act( () => { history.push('/pagina-que-nao-existe/') });
+
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
+
+describe('teste do componente about', () => {
+  it('deve renderizar o componente About (apenas componente)', () => {
+    renderWithRouter(<About />);
+  
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});
