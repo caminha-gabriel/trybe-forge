8bdc95affb3ca5e5c171cf2c900de9f7b1a2713d
Caminha <gabrielcaminha255@gmail.com>
2022-08-08 13:24:24 -0300

    feat: index file with mehods added

---
+++ back_end/bloco-23-restful-architecture/dia-1-camada-model/fixacao/model_example/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const Author = require('./models/Author');
+const Book = require('./models/Book');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Author not found'});
+
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Invalid data'});
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Author created successfully'});
+});
+
+app.get('/books', async (req, res) => {
+  const { author_id } = req.query;
+
+  const books = (author_id)
+  ? await Book.getByAuthorId(author_id)
+  : await Book.getAll();
+
+  res.status(200).json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const book = await Book.findById(id);
+
+  if (!book) return res.status(404).json({ message: 'Book not found'});
+
+  res.status(200).json(book);
+});
+
+app.post('/books', async (req, res) => {
+  const { title, author_id } = req.body;
+
+  if (! await Book.isValid(title, author_id)) {
+    return res.status(400).json({ message: 'Invalid data'});
+  }
+
+  await Book.create(title, author_id);
+
+  res.status(201).json({ message: 'Book created successfully'});
+})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Ouvindo a porta ${PORT}`);
+});
