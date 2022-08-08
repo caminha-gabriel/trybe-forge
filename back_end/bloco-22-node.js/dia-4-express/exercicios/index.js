b21ff1aad60a5851409f77e5bb66330c8a0395c4
Caminha <gabrielcaminha255@gmail.com>
2022-08-08 14:07:55 -0300

    feat: simpsons related methods added

--- back_end/bloco-22-node.js/dia-4-express/exercicios/index.js
+++ back_end/bloco-22-node.js/dia-4-express/exercicios/index.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
 const PORT = 3001;
+const simpsonsFile = './simpsons.json';
 
 app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.get('/simpsons', async (_req, res) => {
+  const content = await fs.readFile(simpsonsFile, 'utf8');
+  const parsedContent = JSON.parse(content);
+
+  if (!parsedContent || parsedContent === []) return res.status(500).json({ message: 'Any Simpson found' });
+
+  res.status(200).json(JSON.parse(content));
+});
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+  const content = await fs.readFile(simpsonsFile, 'utf8');
+  const parsedContent = JSON.parse(content);
+  const requestedSimpson = parsedContent.find((simpson) => simpson.id == id);
+
+  if (!requestedSimpson || requestedSimpson === '') return res.status(404).json({ message: 'Simpson not found'});
+
+  res.status(200).json(requestedSimpson);
+});
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+  const newSimpson = { id: id.toString(), name };
+  const content = await fs.readFile(simpsonsFile, 'utf8');
+  const parsedContent = JSON.parse(content);
+  const newContent = [...parsedContent, newSimpson];
+  
+  if ((!id || id === '') || (!name || name === '')) {
+    return res.status(400).json({ message: 'Invalid data'});
+  }
+
+  if (parsedContent.some((simpson) => simpson.id === id)) {
+    return res.status(409).json({ message: 'Id already exists' });
+  }
+  
+  await fs.writeFile(simpsonsFile, JSON.stringify(newContent));
+
+  res.status(204).end();
 });
 
 app.post('/hello', (req, res) => {
