7474dcd0d9b7b3ef8578c083a58e434ce0289ba1
Caminha <gabrielcaminha255@gmail.com>
2022-08-24 03:51:02 -0300

    feat: created post method  function and its validation for cepService

--- back_end/bloco-23-restful-architecture/dia-2-camadas-service-e-controller/exercicios/cep-lookup-api/services/cepService.js
+++ back_end/bloco-23-restful-architecture/dia-2-camadas-service-e-controller/exercicios/cep-lookup-api/services/cepService.js
@@ -1,10 +1,24 @@
 const cepModel = require('../models/cepModel');
+const Joi = require('joi');
 
 const validateCep = (cep) => {
   const cepRegex = /^\d{5}-?\d{3}$/;
 
   return cep.match(cepRegex);
 };
+
+const validateNewCep = (cep, logradouro, bairro, localidade, uf) => {
+  const cepRegex = /^\d{5}-?\d{3}$/;
+
+  const { error } = Joi.object({
+    cep: Joi.string().not().empty().required().pattern(new RegExp(cepRegex)),
+    logradouro: Joi.string().not().empty().required(),
+    bairro: Joi.string().not().empty().required(),
+    localidade: Joi.string().not().empty().required(),
+    uf: Joi.string().not().empty().required().length(2),
+  }).validate({ cep, logradouro, bairro, localidade, uf });
+  return error;
+}
 
 const getByCep = async (cep) => {
   const cepIsValid = validateCep(cep);
@@ -37,6 +51,40 @@ const getByCep = async (cep) => {
   };
 };
 
+const addCep = async (cep, logradouro, bairro, localidade, uf) => {
+  const newCepError = validateNewCep(cep, logradouro, bairro, localidade, uf);
+
+  if (newCepError) {
+    return {
+      code: 400,
+      message: newCepError.message,
+    }
+  };
+
+  const cepAlreadyExists = await cepModel.getByCep(cep);
+
+  if (cepAlreadyExists) {
+    return {
+      code: 409,
+      message: 'CEP already exists'
+    }
+  }
+
+  await cepModel.addCep(cep, logradouro, bairro, localidade, uf);
+
+  return {
+    code: 201,
+    content: {
+      cep,
+      logradouro,
+      bairro,
+      localidade,
+      uf,
+    },
+  };
+};
+
 module.exports = {
   getByCep,
+  addCep,
 }
